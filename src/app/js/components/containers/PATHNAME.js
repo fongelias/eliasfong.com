6abdc7c7cfb773c742f3ef880fb79a9c18a1611c
fongelias <fongelias@gmail.com>
2020-02-13 12:58:53 -0500

    consolidated PATHNAME with NavBar

--- src/app/js/components/containers/PATHNAME.js
+++
@@ -1,6 +0,0 @@
-export const PATHNAME = {
-	LANDING_PAGE: '/',
-	ABOUT_PAGE: '/About',
-	FAKE_NEWS_PAGE: '/FakeNews',
-	PROJECTS_PAGE: '/Projects',
-}
--- src/app/js/components/presentational/NavBar.jsx
+++ src/app/js/components/presentational/NavBar.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
-import { PATHNAME } from './PATHNAME';
+export const PATHNAME = {
+	LANDING_PAGE: '/',
+	ABOUT_PAGE: '/About',
+	FAKE_NEWS_PAGE: '/FakeNews',
+	PROJECTS_PAGE: '/Projects',
+}
 
 export const NavBar = () => (
 	<nav>
