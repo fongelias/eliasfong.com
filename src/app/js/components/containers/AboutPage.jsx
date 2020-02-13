942c78fb4434441129e020150fc5c30fa2557db3
fongelias <fongelias@gmail.com>
2020-02-13 17:20:32 -0500

    added missing dependency from about page

--- src/app/js/components/containers/AboutPage.jsx
+++ src/app/js/components/containers/AboutPage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { NavBar, PATHNAME } from '../presentational';
-
 
 
 export class AboutPage extends Component {
